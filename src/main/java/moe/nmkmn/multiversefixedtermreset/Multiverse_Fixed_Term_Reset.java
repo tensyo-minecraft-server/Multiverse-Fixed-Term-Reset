e94320a9b6e15224a033a95e80fe847765ad38a3
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-01-10 20:21:19 +0900

    First Commit

---
+++ src/main/java/moe/nmkmn/multiversefixedtermreset/Multiverse_Fixed_Term_Reset.java
@@ -0,0 +1,47 @@
+package moe.nmkmn.multiversefixedtermreset;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+
+
+public final class Multiverse_Fixed_Term_Reset extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+
+        new BukkitRunnable() {
+            boolean executed = false;
+
+            @Override
+            public void run() {
+                Date date = new Date();
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+                if (dayOfWeek == Calendar.SATURDAY && hour == 4 && minute == 0 && !executed) {
+                    getLogger().info("world reset start");
+                    executed = true;
+                    Objects.requireNonNull(core).getMVWorldManager().regenWorld("world_1_o", true, true, null, true);
+                    Objects.requireNonNull(core).getMVWorldManager().regenWorld("world_1_n", true, true, null, true);
+                    Objects.requireNonNull(core).getMVWorldManager().regenWorld("world_1_e", true, true, null, true);
+                }
+
+                if (dayOfWeek == Calendar.SATURDAY && hour == 4 && minute == 1 && executed) {
+                    executed = false;
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);
+    }
+}
